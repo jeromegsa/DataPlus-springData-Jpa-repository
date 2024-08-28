c8147567bb600f05517ecc12831e0c026f0d0d7c
jeromegsa <jeromegbossa3@gmail.com>
2024-08-28 17:07:40 +0100

    initialisation

---
+++ src/main/java/bj/highfiveuniversity/dataplus/DataplusApplication.java
@@ -0,0 +1,13 @@
+package bj.highfiveuniversity.dataplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bj/highfiveuniversity/dataplus/DataplusApplicationTests.java
@@ -0,0 +1,13 @@
+package bj.highfiveuniversity.dataplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
